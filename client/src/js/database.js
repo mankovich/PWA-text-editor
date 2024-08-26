a26f004b94d76a90ec16e5b999607e67efd72d12
Joey M <172998670+mankovich@users.noreply.github.com>
2024-08-26 13:05:01 -0400

    code updates ready to test

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,45 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('put to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  //create connection to the database and version
+  const jateDb = await openDB('jate', 1);
+
+  //create a new transaction and specify the database and data privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  //open the desired object store
+  const store = tx.objectStore('jate');
+
+  //use the FIXME:
+  const request = store.add(content);
+
+  //get confirmation of the request
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  //create connection to the database and version
+  const jateDb = await openDB('jate', 1);
+
+  //create a new transaction and specify the database and data privileges
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  //open the desired object store
+  const store = tx.objectStore('jate');
+
+  //use FIXME: method to get one and then pass the record trying to get
+  const request = store.getAll();
+
+  //get confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
